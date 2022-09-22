94dfd3c87284c4c65c97944837de5daccf7e36f5
tarunsharma47 <tarungodofwar@gmail.com>
2022-09-23 01:39:24 +0530

    driver path change

--- liberis/Utility/webDriver.cs
+++ liberis/Utility/webDriver.cs
@@ -5,8 +5,8 @@ namespace liberis
 {
     internal class webDriver : PageObjectClass
     {
-        //static String driverPath = @"liberis\dependencies";
-        static String driverPath = @"C:\Users\tarun\source\repos\.NET\liberis\liberis\dependencies";
+        static String driverPath = @"liberis\dependencies";
+        //static String driverPath = @"C:\Users\tarun\source\repos\.NET\liberis\liberis\dependencies";
         internal static ChromeDriver driver = new ChromeDriver(driverPath);
     }
 }
